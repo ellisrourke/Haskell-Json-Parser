07245bf9c2ba93ff878fd4085d3ade5d5deb517e
ellisrourke <ellisrourke@gmail.com>
2020-09-29 17:19:08 +1000

    000
    10101010

--- Question2/JsonParser.hs
+++ Question2/JsonParser.hs
@@ -8,30 +8,106 @@ import ABR.Util.Pos
 import ABR.Parser
 import ABR.Parser.Lexers
 
-data Value =  
-      String
-      | Integer
-      | Double
-      | Object
-    deriving Show
-      
-type Object = [(String,[Value])]
+data Value = Str String
+           | Num Double
+          deriving (Show)
+--           | Number Double
+          
+
+data Object = Object 
+   { identifier :: String
+   , value :: Value
+   } deriving (Show)
+
+makeObject :: String -> Value-> Object
+makeObject i v = (Object i v)
 
 symbolL :: Lexer
 symbolL = literalL '(' <|> literalL ')' <|> literalL '+' <|>
           literalL '-' <|> literalL '*' <|> literalL '/'
 
- 
+-- string Lexer
+myStringL :: Lexer
+myStringL =
+   literalL '"'
+      <&&> (many (satisfyL (/= '"') "") &%> "")
+      <&&> literalL '"'
+   %> "string"
 
+-- float Lexer
+myFloatL :: Lexer
+myFloatL =
+   fixedL
+   <&&> soft (optional (
+         (literalL 'e' <|> literalL 'E')
+         <&&> soft (optional (
+               literalL '-' <|> literalL '+'
+              ))
+         <&&> nofail' "exponent expected." cardinalL
+        ))
+   %> "float"
 
+-- Object parser
+--objectP :: Parser Object
+objectP = 
+      literalP "'{'" "{"
+  <&> tagP "string"
+  <&> literalP "':'" ":"
+  <&> valueP
+  <&> literalP "'}'" "}"
 
+-- Value parser
+valueP :: Parser Value
+valueP = 
+      tagP "float"
+      @> (\(_,n,_) -> Num (read n))
+  <|> tagP "string"
+      @> (\(_,n,_) -> Str (read n))
 
-
-
-
-
+objectL:: Lexer
+objectL = dropWhite $ nofail $ total $ listL 
+   [whitespaceL,symbolL, myFloatL,myStringL]
 
 main :: IO ()
-main = print "test"
+main = do
+   putStr "..."
+   hFlush stdout
+   object <- getLine
+   let error :: Pos -> Msg -> IO ()
+       error (_,col) msg = do
+       putStrLn $ "Error: " ++ msg
+       putStrLn object
+       let colPrime = if col < 0
+             then length object
+             else col
+       putStrLn $ replicate colPrime ' '
+         ++ "^"
+       main
+   let cps = preLex object
+   putStrLn $ "pairs: " ++ show cps
+   case objectL cps of
+      Error pos msg -> error pos msg
+      OK (tlps,_) -> do
+         putStrLn $ "Lexemes: " ++ show tlps
+         case objectP tlps of
+            Error pos msg -> error pos msg
+            OK (cmd,_) -> do
+               putStrLn $ "Command: "
+                  ++ show cmd
+
+
+{--
+let error :: Pos -> Msg -> IO ()
+       error (_,col) msg = do
+       putStrLn $ "Error: " ++ msg
+       putStrLn command
+       let colPrime = if col < 0
+             then length command
+             else col
+       putStrL $ replicate colPrime ' '
+         ++ "^"
+       main
+--}
+
 
 
